693854483d66fc17c85523626c42d7a65411239e
fergdev <fergus.a.hewson@outlook.com>
2025-11-16 04:24:43 +1300

    Initial commit

---
+++ compiler-plugin/test-fixtures/org/jetbrains/kotlin/compiler/plugin/template/services/ExtensionRegistrarConfigurator.kt
@@ -0,0 +1,27 @@
+package org.jetbrains.kotlin.compiler.plugin.template.services
+
+import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
+import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
+import org.jetbrains.kotlin.compiler.plugin.template.SimplePluginRegistrar
+import org.jetbrains.kotlin.compiler.plugin.template.ir.SimpleIrGenerationExtension
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrarAdapter
+import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
+import org.jetbrains.kotlin.test.model.TestModule
+import org.jetbrains.kotlin.test.services.EnvironmentConfigurator
+import org.jetbrains.kotlin.test.services.TestServices
+
+fun TestConfigurationBuilder.configurePlugin() {
+    useConfigurators(::ExtensionRegistrarConfigurator)
+    configureAnnotations()
+}
+
+private class ExtensionRegistrarConfigurator(testServices: TestServices) : EnvironmentConfigurator(testServices) {
+    override fun CompilerPluginRegistrar.ExtensionStorage.registerCompilerExtensions(
+        module: TestModule,
+        configuration: CompilerConfiguration
+    ) {
+        FirExtensionRegistrarAdapter.registerExtension(SimplePluginRegistrar())
+        IrGenerationExtension.registerExtension(SimpleIrGenerationExtension())
+    }
+}
